c93d513250ae3c83c7eaa073f9ee6f118b2fe607
Fabis-li <fabiano_slima@hotmail.com>
2024-07-30 20:30:20 -0300

    criado repositorio base orm

---
+++ ControleDeCinema.Infra/Compartilhado/RepositorioBaseEmOrm.cs
@@ -0,0 +1,72 @@
+﻿using ControleDeCinema.Dominio.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeCinema.Infra.Compartilhado;
+
+public abstract class RepositorioBaseEmOrm<TEntidade> where TEntidade : EntidadeBase
+{
+    protected readonly ControleDeCinemaDbContext dbContext;
+
+    public RepositorioBaseEmOrm(ControleDeCinemaDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    protected abstract DbSet<TEntidade> ObterRegistros();
+
+    public void Inserir(TEntidade registro)
+    {
+        ObterRegistros().Add(registro);
+
+        dbContext.SaveChanges();
+    }
+
+    public virtual bool Editar(TEntidade registroOriginal, TEntidade registroAtualizado)
+    {
+        if (registroOriginal == null || registroAtualizado == null)
+            return false;
+        
+        registroOriginal.AtualizarInformacoes(registroAtualizado);
+        
+        ObterRegistros().Update(registroOriginal);
+        
+        dbContext.SaveChanges();
+
+        return true;
+    }
+
+    public virtual bool Editar(TEntidade registroAtualizado)
+    {
+        if (registroAtualizado == null)
+            return false;
+
+        ObterRegistros().Update(registroAtualizado);
+
+        dbContext.SaveChanges();
+
+        return true;
+    }
+
+    public virtual bool Excluir(TEntidade registro)
+    {
+        if (registro == null)
+            return false;
+
+        ObterRegistros().Remove(registro);
+
+        dbContext.SaveChanges();
+
+        return true;
+    }
+
+    public virtual TEntidade SelecionarPorId(int id)
+    {
+        return ObterRegistros()
+            .FirstOrDefault(m => m.Id == id)!;
+    }
+
+    public virtual List<TEntidade> SelecionarTodos()
+    {
+        return ObterRegistros().ToList();
+    }
+}
