7af8ba49afb379e94a21f30f5f326d7988a3a617
Fabis-li <fabiano_slima@hotmail.com>
2024-07-30 17:36:40 -0300

    criado interface do repositorio base

---
+++ ControleDeCinema.Dominio/Compartilhado/IRepositorioBase.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeCinema.Dominio.Compartilhado
+{
+    public interface IRepositorioBase<TEntidade> where TEntidade : EntidadeBase
+    {
+        void Inseir(TEntidade registro);
+        bool Editar(TEntidade registroOriginal, TEntidade registroAtualizado);
+        bool Excluir(TEntidade registro);
+        TEntidade SelecionarPorId(int id);
+        List<TEntidade> SelecionarTudo();
+    }
+}
