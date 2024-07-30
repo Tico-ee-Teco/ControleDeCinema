6c37a13ab1f37f1ccfa2b33be3d1ae186b33b137
Fabis-li <fabiano_slima@hotmail.com>
2024-07-30 20:29:53 -0300

    referenciado projeto da camada dominio

--- ControleDeCinema.Infra/Compartilhado/ControleDeCinemaDbContext.cs
+++ ControleDeCinema.Infra/Compartilhado/ControleDeCinemaDbContext.cs
@@ -1,6 +1,22 @@
-﻿namespace ControleDeCinema.Infra.Compartilhado
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ControleDeCinema.Infra.Compartilhado
 {
-    public class ControleDeCinemaDbContext
+    public class ControleDeCinemaDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("SqlServer")!;
+
+            optionsBuilder.UseSqlServer(connectionString);
+            
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
