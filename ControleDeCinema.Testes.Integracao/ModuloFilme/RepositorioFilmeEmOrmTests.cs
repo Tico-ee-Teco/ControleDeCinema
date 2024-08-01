b24df4774106027bc3189ad00e71e04a7945f573
Fabis-li <fabiano_slima@hotmail.com>
2024-08-01 14:00:06 -0300

    criado teste para inserir filme

---
+++ ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTests.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeCinema.Infra.Compartilhado;
+
+namespace ControleDeCinema.Testes.Integracao;
+
+[TestClass]
+public class RepositorioFilmeEmOrmTests
+{
+    [TestMethod]
+    public void Deve_inserir_um_filme()
+    {
+        ControleDeCinemaDbContext dbContext = new ControleDeCinemaDbContext();
+
+        var genero = new Genero("Ação");
+        RepositorioGeneroOrm repositorioGenero = new RepositorioGeneroOrm(dbContext);
+        repositorioGenero.Inserir(genero);
+
+        Filme filme = new Filme();
+        RepositorioFilmeOrm repositorioFilme = new RepositorioFilmeOrm(dbContext);
+        repositorioFilme.Inserir(filme);
+        
+        Assert.IsTrue(filme.Id > 0);
+    }
+}
