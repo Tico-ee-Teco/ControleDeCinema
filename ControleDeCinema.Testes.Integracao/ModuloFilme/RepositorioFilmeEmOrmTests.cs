0a796e32ca4fc749655582b335187fb879da5f5b
Fabis-li <fabiano_slima@hotmail.com>
2024-08-01 23:01:44 -0300

    criado testes integracao para o modulo genero

--- ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTests.cs
+++ ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTests.cs
@@ -41,4 +41,95 @@ public class RepositorioFilmeEmOrmTests
         //Assert
         Assert.IsTrue(filme.Id > 0);
     }
+
+    [TestMethod]
+    public void Deve_editar_um_filme()
+    {
+        //Arrange
+        Genero genero = new Genero("Ação");
+
+        repositorioGenero.Inserir(genero);
+
+        Filme filmeOriginal = new Filme("Fui e Ja Volto",genero,DateTime.MinValue, false);
+
+        repositorioFilme.Inserir(filmeOriginal);
+
+        Filme filmeAtualizado = repositorioFilme.SelecionarPorId(filmeOriginal.Id);
+
+        filmeAtualizado.Titulo = "Fui e Voltei";
+
+        //Act
+        repositorioFilme.Editar(filmeOriginal, filmeAtualizado);
+
+        //Assert
+        Assert.AreEqual(filmeOriginal.Titulo, filmeAtualizado.Titulo);
+    }
+
+    [TestMethod]
+    public void Deve_excluir_um_filme()
+    {
+        //Arrange
+        Genero genero = new Genero("Ação");
+
+        repositorioGenero.Inserir(genero);
+
+        Filme filme = new Filme("Fui e Ja Volto",genero,DateTime.MinValue, false);
+
+        repositorioFilme.Inserir(filme);
+
+        //Act
+        repositorioFilme.Excluir(filme);
+
+        //Assert
+        Filme filmeSelecinado = repositorioFilme.SelecionarPorId(filme.Id);
+
+        Assert.IsNull(filmeSelecinado);
+    }
+
+    [TestMethod]
+    public void Deve_selecionar_um_filme_por_id()
+    {
+        //Arrange
+        Genero genero = new Genero("Ação");
+
+        repositorioGenero.Inserir(genero);
+
+        Filme filme = new Filme("Fui e Ja Volto",genero,DateTime.MinValue, false);
+
+        repositorioFilme.Inserir(filme);
+
+        //Act
+        Filme filmeSelecinado = repositorioFilme.SelecionarPorId(filme.Id);
+
+        //Assert
+        Assert.IsNotNull(filmeSelecinado);
+    }
+
+    [TestMethod]
+    public void Deve_selecionar_todos_os_filmes()
+    {
+        //Arrange
+        Genero genero = new Genero("Ação");
+
+        repositorioGenero.Inserir(genero);
+
+        List<Filme> filmesParainseir =
+        [
+            new Filme("Fui e Ja Volto",genero,DateTime.MinValue, false),
+            new Filme("Fui e Voltei",genero,DateTime.MinValue, false),
+            new Filme("Fui e Fiquei", genero, DateTime.MinValue, true)
+
+        ];
+
+        foreach( Filme filme in filmesParainseir)
+            repositorioFilme.Inserir(filme);
+
+        //Act
+        List<Filme> filmesSelecionados = repositorioFilme.SelecionarTudo();
+
+        //Assert
+        Assert.AreEqual(3, filmesSelecionados.Count);
+        CollectionAssert.AreEqual(filmesParainseir, filmesSelecionados);
+        
+    }
 }
---
+++ ControleDeCinema.Testes.Integracao/ModuloGenero/RepositorioGeneroemOrmTests.cs
@@ -0,0 +1,70 @@
+﻿using ControleDeCinema.Dominio.ModuloGenero;
+using ControleDeCinema.Infra.Compartilhado;
+using ControleDeCinema.Infra.ModuloGenero;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloGenero;
+
+[TestClass]
+[TestCategory("Testes de Integração de Genero")]
+public class RepositorioGeneroemOrmTests
+{
+    RepositorioGeneroEmOrm repositorioGenero;
+    ControleDeCinemaDbContext dbContext;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        dbContext = new ControleDeCinemaDbContext(); 
+        repositorioGenero = new RepositorioGeneroEmOrm(dbContext);
+
+        dbContext.Generos.RemoveRange(dbContext.Generos);
+    }
+
+    [TestMethod]
+    public void Deve_inserir_um_genero()
+    {
+        //Arrange
+        Genero genero = new Genero("Ação");
+
+        //Act
+        repositorioGenero.Inserir(genero);
+        
+        //Assert
+        Assert.IsTrue(genero.Id > 0);
+    }
+
+    [TestMethod]
+    public void Deve_editar_um_genero()
+    {
+        //Arrange
+        Genero generoOriginal = new Genero("Ação");
+
+        repositorioGenero.Inserir(generoOriginal);
+
+        Genero generoAtualizado = repositorioGenero.SelecionarPorId(generoOriginal.Id);
+
+        generoAtualizado.Nome = "Ação e Aventura";
+
+        //Act
+        repositorioGenero.Editar(generoOriginal, generoAtualizado);
+
+        //Assert
+        Assert.AreEqual(generoOriginal.Nome, generoAtualizado.Nome);
+    }
+
+    [TestMethod]
+    public void Deve_excluir_um_genero()
+    {
+        //Arrange
+        Genero genero = new Genero("Ação");
+
+        repositorioGenero.Inserir(genero);
+
+        //Act
+        repositorioGenero.Excluir(genero);
+
+        //Assert
+        Assert.IsNull(repositorioGenero.SelecionarPorId(genero.Id));
+    }
+}
+
