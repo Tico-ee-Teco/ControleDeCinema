cdb168c8fe4408e06ad657b0095f0dd442fa7459
Fabis-li <fabiano_slima@hotmail.com>
2024-08-02 19:49:17 -0300

    criado controler filme e genero

---
+++ ControleDeCinema.WebApp/Controllers/FilmeController.cs
@@ -0,0 +1,77 @@
+﻿using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGenero;
+using ControleDeCinema.Infra.Compartilhado;
+using ControleDeCinema.Infra.ModuloFilme;
+using ControleDeCinema.Infra.ModuloGenero;
+using ControleDeCinema.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ControleDeCinema.WebApp.Controllers
+{
+    public class FilmeController : Controller
+    {
+        public ViewResult Listar()
+        {
+            var db = new ControleDeCinemaDbContext();
+            var repositorioFilme = new RepositorioFilmeEmOrm(db);
+
+            var filmes = repositorioFilme.SelecionarTodos();
+
+            var listarFilmesVm = filmes
+                .Select(f => new ListarFilmeViewModel
+                {
+                    Id = f.Id,
+                    Nome = f.Titulo,
+                    Duracao = f.Duracao,
+                    Estreia = f.Estreia,
+                });
+
+            return View(listarFilmesVm);
+        }
+
+        public ViewResult Inserir()
+        {
+            var db = new ControleDeCinemaDbContext();
+            var repositorioGenero = new RepositorioGeneroEmOrm(db);
+
+            var generosDeFilme = repositorioGenero.SelecionarTodos();
+
+            var inserirFilmeVm = new InserirFilmeViewModel
+            {
+                Generos = generosDeFilme
+                    .Select(g => new SelectListItem
+                    {
+                        Value = g.Id.ToString(),
+                        Text = g.Nome
+                    })
+            };
+
+            return View(inserirFilmeVm);
+        }
+
+        [HttpPost]
+        public ViewResult Inserir(InserirFilmeViewModel inserirFilmeVm)
+        {
+            var db = new ControleDeCinemaDbContext();
+            var repositorioFilme = new RepositorioFilmeEmOrm(db);
+            var repositorioGenero = new RepositorioGeneroEmOrm(db);
+
+            var genero = repositorioGenero.SelecionarPorId(inserirFilmeVm.IdGenero);
+
+            var filme = new Filme(inserirFilmeVm.Nome, genero, inserirFilmeVm.Duracao, inserirFilmeVm.Estreia);
+
+            repositorioFilme.Inserir(filme);
+
+            HttpContext.Response.StatusCode = 201;
+
+            var notificacaoVm = new NotificacaoViewModel
+            {
+                Mensagem = $"O registro com o ID [{filme.Id}] foi cadastrado com sucesso!",
+                LinkRedirecionamento = "/filme/listar"
+            };
+
+            return View();
+        }
+    }
+}
---
+++ ControleDeCinema.WebApp/Controllers/GeneroController.cs
@@ -0,0 +1,97 @@
+﻿using ControleDeCinema.Dominio.ModuloGenero;
+using ControleDeCinema.Infra.Compartilhado;
+using ControleDeCinema.Infra.ModuloGenero;
+using ControleDeCinema.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeCinema.WebApp.Controllers
+{
+    public class GeneroController : Controller
+    {
+        public ViewResult Listar()
+        {
+            var db = new ControleDeCinemaDbContext();
+            var repositorioGenero = new RepositorioGeneroEmOrm(db);
+
+            var generos = repositorioGenero.SelecionarTodos();
+
+            var listarGenerosVm = generos
+                .Select(g => new ListarGeneroViewModel
+                {
+                    Id = g.Id,
+                    Nome = g.Nome
+                });
+
+            return View(listarGenerosVm);
+        }
+
+        public ViewResult Inserir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Inserir(InserirGeneroViewModel inserirGeneroVm)
+        {
+            var db = new ControleDeCinemaDbContext();
+            var repositorioGenero = new RepositorioGeneroEmOrm(db);
+
+            var genero = new Genero(inserirGeneroVm.Nome);
+
+            repositorioGenero.Inserir(genero);
+
+            HttpContext.Response.StatusCode = 201;
+
+            var notificacaoVm = new NotificacaoViewModel
+            {
+                Mensagem = $"O registro com o ID [{genero.Id}] foi cadastrado com sucesso!",
+                LinkRedirecionamento = "/genero/listar"
+            };
+
+            return View("mensagens", notificacaoVm);
+        }
+
+        public ViewResult Editar(int id)
+        {
+            var db = new ControleDeCinemaDbContext();
+            var repositorioGenero = new RepositorioGeneroEmOrm(db);
+
+            var genero = repositorioGenero.SelecionarPorId(id);
+
+            var editarGeneroVm = new EditarGeneroViewModel
+            {
+                Id = genero.Id,
+                Nome = genero.Nome
+            };
+
+            return View(editarGeneroVm);
+        }
+
+        [HttpPost]
+        public ViewResult Editar(EditarGeneroViewModel editarGeneroVm)
+        {
+            var db = new ControleDeCinemaDbContext();
+            var repositorioGenero = new RepositorioGeneroEmOrm(db);
+
+            var generoOriginal = repositorioGenero.SelecionarPorId(editarGeneroVm.Id);
+
+            generoOriginal.Nome = editarGeneroVm.Nome;
+
+            repositorioGenero.Editar(generoOriginal);
+
+            HttpContext.Response.StatusCode = 200;
+
+            var notificacaoVm = new NotificacaoViewModel
+            {
+                Mensagem = $"O registro com o ID [{generoOriginal.Id}] foi atualizado com sucesso!",
+                LinkRedirecionamento = "/genero/listar"
+            };
+
+            return View("mensagens", notificacaoVm);
+        }
+
+        
+    }
+
+  
+}
