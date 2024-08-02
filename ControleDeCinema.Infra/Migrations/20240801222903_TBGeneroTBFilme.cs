724670fe0c44f8a6d4845f9c7ca7e6452a9daffc
Fabis-li <fabiano_slima@hotmail.com>
2024-08-01 22:03:30 -0300

    testado inseir filme

---
+++ ControleDeCinema.Infra/Migrations/20240801222903_TBGeneroTBFilme.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleDeCinema.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class TBGeneroTBFilme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBGenero",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBGenero", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBFilme",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Genero_Id = table.Column<int>(type: "int", nullable: false),
+                    Duracao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Estreia = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBFilme", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBFilme_TBGenero_Genero_Id",
+                        column: x => x.Genero_Id,
+                        principalTable: "TBGenero",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBFilme_Genero_Id",
+                table: "TBFilme",
+                column: "Genero_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBFilme");
+
+            migrationBuilder.DropTable(
+                name: "TBGenero");
+        }
+    }
+}
--- ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTests.cs
+++ ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTests.cs
@@ -1,28 +1,44 @@
-﻿using ControleDeCinema.Dominio;
-using ControleDeCinema.Dominio.ModuloFilme;
+﻿using ControleDeCinema.Dominio.ModuloFilme;
 using ControleDeCinema.Dominio.ModuloGenero;
 using ControleDeCinema.Infra.Compartilhado;
 using ControleDeCinema.Infra.ModuloFilme;
 using ControleDeCinema.Infra.ModuloGenero;
 
-namespace ControleDeCinema.Testes.Integracao;
+namespace ControleDeCinema.Testes.Integracao.moduloFilme;
 
 [TestClass]
+[TestCategory("Testes de Integração de Filme")]
 public class RepositorioFilmeEmOrmTests
 {
+    RepositorioFilmeEmOrm repositorioFilme;
+    RepositorioGeneroEmOrm repositorioGenero;
+    ControleDeCinemaDbContext dbContext;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+       dbContext = new ControleDeCinemaDbContext(); 
+       repositorioFilme = new RepositorioFilmeEmOrm(dbContext);
+       repositorioGenero = new RepositorioGeneroEmOrm(dbContext);
+
+       dbContext.Filmes.RemoveRange(dbContext.Filmes);
+       dbContext.Generos.RemoveRange(dbContext.Generos);
+    }
+
     [TestMethod]
     public void Deve_inserir_um_filme()
     {
-        ControleDeCinemaDbContext dbContext = new ControleDeCinemaDbContext();
+        //Arrange
+        Genero genero = new Genero("Ação");
 
-        var genero = new Genero("Ação");
-        RepositorioGeneroEmOrm repositorioGenero = new RepositorioGeneroEmOrm(dbContext);
         repositorioGenero.Inserir(genero);
 
-        Filme filme = new Filme();
-        RepositorioFilmeEmOrm repositorioFilme = new RepositorioFilmeEmOrm(dbContext);
-        repositorioFilme.Inserir(filme);
+        Filme filme = new Filme("Fui e Ja Volto",genero,DateTime.MinValue, false);
 
+        //Act
+        repositorioFilme.Inserir(filme);
+        
+        //Assert
         Assert.IsTrue(filme.Id > 0);
     }
 }
