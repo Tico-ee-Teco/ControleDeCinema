b53efe1a043edb83fb73c2223d3d87ba5c4ce202
Fabis-li <fabiano_slima@hotmail.com>
2024-08-03 14:11:24 -0300

    criado repositorio para testes

---
+++ ControleDeCinema.Testes.Integracao/ModuloFuncionario/RepositorioFuncionarioEmOrmTests.cs
@@ -0,0 +1,18 @@
+﻿using ControleDeCinema.Infra.Compartilhado;
+using ControleDeCinema.Infra.ModuloFuncionario;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloFuncionario;
+
+[TestClass]
+[TestCategory("Testes de Integração de Funcionario")]
+public class RepositorioFuncionarioEmOrmTests
+{
+    RepositorioFuncionarioEmOrm repositorioFuncionario;
+    ControleDeCinemaDbContext dbContext;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        
+    }
+}
