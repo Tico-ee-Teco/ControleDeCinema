c1d7947176b9f06d6c4d9b276088639b9cde8dee
Fabis-li <fabiano_slima@hotmail.com>
2024-08-03 14:12:07 -0300

    criado repositorio

---
+++ ControleDeCinema.Infra/ModuloFuncionario/RepositorioFuncionarioEmOrm.cs
@@ -0,0 +1,16 @@
+﻿using ControleDeCinema.Dominio;
+using ControleDeCinema.Infra.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeCinema.Infra.ModuloFuncionario;
+
+public class RepositorioFuncionarioEmOrm : RepositorioBaseEmOrm<Funcionario>,IRepositorioFuncionario
+{
+    public RepositorioFuncionarioEmOrm(ControleDeCinemaDbContext dbContext) : base(dbContext)
+    {
+    }
+    protected override DbSet<Funcionario> ObterRegistros()
+    {
+        return dbContext.Funcionarios;
+    }
+}
