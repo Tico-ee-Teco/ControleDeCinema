1feac180689e004ca35b8d10368a57764ab72d5f
Fabis-li <fabiano_slima@hotmail.com>
2024-08-04 17:52:41 -0300

    criado model e view

---
+++ ControleDeCinema.WebApp/Models/SalaViewModels.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace ControleDeCinema.WebApp.Models
+{
+    public class ListarSalaViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Capacidade { get; set; }
+        
+    }
+
+    public class InserirSalaViewModel
+    {
+        public string Nome { get; set; }
+        public int Capacidade { get; set; }
+    }
+
+    public class EditarSalaViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Capacidade { get; set; }
+    }
+
+    public class ExcluirSalaViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Capacidade { get; set; }
+    }
+}
