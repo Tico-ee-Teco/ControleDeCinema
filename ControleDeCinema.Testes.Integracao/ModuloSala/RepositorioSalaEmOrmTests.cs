b8748ff416a193ca6a6080c0abfa58917c020219
Fabis-li <fabiano_slima@hotmail.com>
2024-08-04 16:36:11 -0300

    criado testes de integracao  inserir e editar uma sala

---
+++ ControleDeCinema.Testes.Integracao/ModuloSala/RepositorioSalaEmOrmTests.cs
@@ -0,0 +1,61 @@
+﻿using ControleDeCinema.Dominio;
+using ControleDeCinema.Infra.Compartilhado;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloSala
+{
+    [TestClass]
+    [TestCategory("Testes de integracao de Sala")]
+    public class RepositorioSalaEmOrmTests
+    {
+        RepositorioSalaEmOrm repositorioSalaEmOrm;
+        ControleDeCinemaDbContext dbContext;
+
+        [TestInitialize]
+        public void ConfigurarTestes()
+        {
+            dbContext = new ControleDeCinemaDbContext();
+            repositorioSala = new RepositorioSalaEmOrm(dbContext);
+
+            dbContext.Salas.RemoveRange(dbContext.Salas);
+        }
+
+        [TestMethod]
+        public void Deve_Inserir_Sala()
+        {
+            // Arrange
+            var sala = new Sala("Sala 1", 100);
+
+            // Act
+            repositorioSala.Inserir(sala);
+
+            // Assert
+            var salaInserida = dbContext.Salas.FirstOrDefault();
+            Assert.IsNotNull(salaInserida);
+            Assert.AreEqual(sala.Nome, salaInserida.Nome);
+            Assert.AreEqual(sala.QuantidadeDeLugares, salaInserida.QuantidadeDeLugares);
+        }
+
+        [TestMethod]
+        public void Deve_Editar_Sala()
+        {
+            // Arrange
+            Sala salaOriginal = new Sala("Sala 1", 100);
+
+            repositorioSala.Inserir(salaOriginal);
+
+            Sala salaEditada = repositorioSala.SelecionarPorid(salaOriginal.Id);
+
+            salaEditada.Nome = "Sala 2";
+            salaEditada.QuantidadeDeLugares = 200;
+
+            // Act
+            repositorioSala.Editar(salaEditada);
+
+            // Assert
+            var salaEditadaNoBanco = dbContext.Salas.FirstOrDefault();
+            Assert.IsNotNull(salaEditadaNoBanco);
+            Assert.AreEqual(salaEditada.Nome, salaEditadaNoBanco.Nome);
+            Assert.AreEqual(salaEditada.QuantidadeDeLugares, salaEditadaNoBanco.QuantidadeDeLugares);
+        }
+    }
+}
