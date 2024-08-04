9638280b860b3510482a03468b541f2d9c64f70a
Fabis-li <fabiano_slima@hotmail.com>
2024-08-04 17:51:41 -0300

    criado migracoes para o banco, repositorio em orm

---
+++ ControleDeCinema.Infra/ModuloSala/RepositorioSalaEmOrm.cs
@@ -0,0 +1,18 @@
+﻿using ControleDeCinema.Dominio.ModulosSala;
+using ControleDeCinema.Infra.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeCinema.Infra.ModuloSala
+{
+    public class RepositorioSalaEmOrm : RepositorioBaseEmOrm<Sala>, IRepositorioSala
+    {
+        public RepositorioSalaEmOrm(ControleDeCinemaDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override DbSet<Sala> ObterRegistros()
+        {
+            return dbContext.Salas;
+        }
+    }
+}
