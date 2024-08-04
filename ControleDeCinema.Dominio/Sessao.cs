ea4171bdc25bf4a98f42da4aa888098932d0dd02
Fabis-li <fabiano_slima@hotmail.com>
2024-08-04 17:53:01 -0300

    ajustado a classe sessao

--- ControleDeCinema.Dominio/Sessao.cs
+++ ControleDeCinema.Dominio/Sessao.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModulosSala;
 
 namespace ControleDeCinema.Dominio
 {
