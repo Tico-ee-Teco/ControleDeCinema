c10e0de2d13e1de428383b0266c135232ba049ad
Fabis-li <fabiano_slima@hotmail.com>
2024-08-06 22:54:36 -0300

    alterado selecionar por id

--- ControleDeCinema.Infra/ModuloSessao/RepositorioSessaoEmOrm.cs
+++ ControleDeCinema.Infra/ModuloSessao/RepositorioSessaoEmOrm.cs
@@ -14,5 +14,23 @@ namespace ControleDeCinema.Infra.ModuloSessao
         {
             return dbContext.Sessoes;
         }
+
+        public override Sessao SelecionarPorId(int id)
+        {
+            return dbContext.Sessoes
+                .Include(s => s.Filme)
+                .Include(s => s.Sala)
+                .FirstOrDefault(s => s.Id == id);
+        }
+
+        public List<IGrouping<string, Sessao>> ObterSessoesAgrupadas()
+        {
+            return ObterRegistros()
+                .Include(s => s.Filme)
+                .ThenInclude(f => f.Genero)
+                .Include(s => s.Sala)
+                .GroupBy(s => s.Filme.Titulo)
+                .ToList();
+        }
     }
 }
